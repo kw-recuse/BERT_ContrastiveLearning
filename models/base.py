3e3fc551a88e78156ae36c42c58d71a10f8bb5e9
seungjun-green <lsj3285007@gmail.com>
2025-03-01 12:28:47 +0900

    use LongformerModel instead of LongformerMLM

--- models/base.py
+++ models/base.py
@@ -1,9 +1,9 @@
-from transformers import LongformerTokenizer, LongformerForMaskedLM
+from transformers import LongformerTokenizer, LongformerModel
 
 def load_tokenizer_and_model(model_name):
     try:
         tokenizer = LongformerTokenizer.from_pretrained(model_name) 
-        model = LongformerForMaskedLM.from_pretrained(model_name)
+        model = LongformerModel.from_pretrained(model_name)
         return tokenizer, model
     except:
         raise ValueError(f"{model_name} does not exist.")
