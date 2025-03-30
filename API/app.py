f0285892d785a967d42403e626173d9e362d3c36
seungjun-green <lsj3285007@gmail.com>
2025-03-30 16:07:55 +0900

    add text cleaning and onnx

--- API/app.py
+++ API/app.py
@@ -1,9 +1,14 @@
-# Run the requirements.txt first (transformers, torch, etc.)
+import re
 from flask import Flask, request, jsonify
 from transformers import AutoTokenizer, AutoModel
 import torch
 
 app = Flask(__name__)
+
+def clean_text(text):
+    text = re.sub(r'\s+', ' ', text).strip()
+    text = re.sub(r'[^\w\s]', '', text)
+    return text
 
 # load model and tokenizer
 tokenizer = AutoTokenizer.from_pretrained("recuse/distiluse-base-multilingual-cased-v2-mean-dataV1")
@@ -23,9 +28,18 @@ def embed():
     
     if not text:
         return jsonify({"error": "Text is required"}), 400
+    
+    cleaned_text = clean_text(text)
 
-    embedding = get_embedding(text)
+    embedding = get_embedding(cleaned_text)
     return jsonify({"embedding": embedding})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+"""
+resume_str = "...."
+curl -X POST http://127.0.0.1:5000/embed \
+-H "Content-Type: application/json" \
+-d '{"text": resume_str}'
+"""
---
+++ API/onnnx_app.py
@@ -0,0 +1,44 @@
+import re
+from flask import Flask, request, jsonify
+from transformers import AutoTokenizer
+import onnxruntime
+import numpy as np
+
+app = Flask(__name__)
+
+def clean_text(text):
+    text = re.sub(r'\s+', ' ', text).strip()
+    text = re.sub(r'[^\w\s]', '', text)
+    return text
+
+
+model_name = "recuse/distiluse-base-multilingual-cased-v2-mean-dataV1"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+onnx_session = onnxruntime.InferenceSession("distiluse-base-multilingual-cased-v2-mean-dataV1.onnx", providers=["CPUExecutionProvider"])
+
+def get_embedding_onnx(text):
+    cleaened_text = clean_text(text)
+    inputs = tokenizer(cleaened_text, return_tensors="np", padding=True, truncation=True)
+    
+    onnx_inputs = {
+        "input_ids": inputs['input_ids'].astype(np.int64),
+        "attention_mask": inputs['attention_mask'].astype(np.int64)
+    }
+    
+    onnx_outputs = onnx_session.run(None, onnx_inputs)
+    embeddings = np.mean(onnx_outputs[0], axis=1)
+    return embeddings.tolist()
+
+@app.route('/embed', methods=['POST'])
+def embed():
+    data = request.json
+    text = data.get("text", "")
+    
+    if not text:
+        return jsonify({"error": "Text is required"}), 400
+
+    embedding = get_embedding_onnx(text)
+    return jsonify({"embedding": embedding})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5004)
